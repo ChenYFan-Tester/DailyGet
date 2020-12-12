de704e9454ff52d18117e967cddfe5f0888c9971
ChenYFan <ChenYFan@users.noreply.github.com>
2020-12-12 01:32:28 +0000

    deploy: 08670eb89f7ee4ac8d3ecd5530155140ef2eccbf

--- bingpic/bing.js
+++ bingpic/bing.js
@@ -1,1 +1,1 @@
-document.write("Sleeping Arctic fox (© Menno Schaefer/Getty Images)");
+document.write("Queen of the Andes plants with the Cordillera Blanca massif in the background, Peru (© Cyril Ruoso/Minden Pictures)");
