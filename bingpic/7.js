b93bdfc03224fedc83fa7724eab010f670ad05d2
ChenYFan <ChenYFan@users.noreply.github.com>
2020-12-19 01:47:17 +0000

    deploy: 08670eb89f7ee4ac8d3ecd5530155140ef2eccbf

--- bingpic/7.js
+++ bingpic/7.js
@@ -1,1 +1,1 @@
-﻿document.write("Queen of the Andes plants with the Cordillera Blanca massif in the background, Peru (© Cyril Ruoso/Minden Pictures)");
+﻿document.write("Poinsettia leaf close-up (© Charles Floyd/Alamy)");
